0306799ab0c4d836091e8fe6ba0a4f4a5cd5e97a
Georgi K <georgiknox@gmail.com>
2014-07-10 23:18:51 -0400

    refactored

--- marys-functional.clj
+++ marys-functional.clj
@@ -11,7 +11,7 @@
 (squares (range 1 5))
 
 ;(defn squares-2 [x]
- ; map (fn[x] (* n n)) x)
+; map (fn[x] (* n n)) x)
 
 (defn get-random []
   (rand-nth ["this" "is" "random"]))
@@ -19,19 +19,12 @@
 
 (defn replace-with-random-name
   [coll]
-    (map #(get-random %) coll))
+  (map #(get-random %) coll))
 
 (replace-with-random-name ["mary" "isla" "sam" "dean" "person"])
 
 
 ;; map -> exercise 1
-(defn exercise-1
-  [orig-names]
-    (map #(hash %) orig-names))
-
-(exercise-1 ["mary" "isla" "sam" "dean"])
-
-
 ;; count number of times word appears in sentence
 (use '[clojure.string :only (split)])
 
@@ -43,18 +36,77 @@
             (if (= word substring)
               (+ curr-count 1)
               curr-count))
-            0 (split-words sentence)))
-
-(word-count "Sam read a story to Sam and Isla." "Sam")
-(word-count "Sam Sam" "Sam")
+          0 (split-words sentence)))
 
 ;; count the number of "sams" in given list of sentences
 (defn count-the-sams
   [sentences]
-    (reduce (fn [current-count sentence]
-              (+ current-count (word-count sentence "Sam")))
-            0 sentences))
-
+  (reduce (fn [current-count sentence]
+            (+ current-count (word-count sentence "Sam")))
+          0 sentences))
+;; update reduce to simply count the number of times same appears (don't worry about split)
+;; trying using (apply + indiviual-count)
 (count-the-sams ["Mary read a story to Sam and Isla." "Isla cuddled Sam" "Sam chortled."])
+
+
+;; exercise-2
+;; find the average height of collection of people. only calculate for those that have a height
+
+;; try to condense numb methods, single let.
+
+(def people [{:name "Mary" :height 160 }
+             {:name "Isla" :height 80}
+             {:name "Sam"}])
+
+(defn filter-by-height
+  "Filters collection for those who have height"
+  [coll]
+  (filter :height coll))
+
+(defn total-num-people-with-height [coll]
+  (count (filter-by-height coll)))
+
+(defn total-height [coll]
+  (reduce (fn [curr-count person]
+            (+ curr-count (:height person))) 0 coll))
+
+(defn average-height-1 [coll]
+  (double (/ (total-height (filter-by-height coll))
+             (total-num-people-with-height coll))))
+
+(average-height-1 people)
+
+
+;; take 2!
+;; user thread macro
+(def people [{:name "Mary" :height 160}
+             {:name "Isla" :height 80}
+             {:name "Sam"}])
+
+(defn average-height [coll]
+  (let [filtered-by-height (filter :height coll)
+        num-with-height (count filtered-by-height)]
+    (if (> num-with-height 0)
+      (double (/ (->> filtered-by-height
+                      (map :height)
+                      (apply +))
+                 num-with-height))
+      nil)))
+
+(average-height people)
+
+
+;; find last element of a list
+(defn find-last [coll]
+  (let [[head & tail :as all] coll]
+    (if (empty? tail)
+    head
+    (recur tail))))
+
+(find-last [ 1 2 3 4])
+
+
+
+
 
 
